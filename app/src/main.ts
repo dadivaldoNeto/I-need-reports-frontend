0216d62fff8e7ce424c16632ec72ad8890e8132e
Dadivaldo <dadivaldomendonca010@gmail.com>
2026-07-05 11:46:33 +0100

    feat: UI login and register pages

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,10 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
-
 import './css/index.css';
+import { getFormType, loginView, processForm } from './pages/login';
 
-import { loginView } from './pages/login';
+const APP = document.querySelector("#app")!;
 
+const routes = {
+	'/login' : loginView(getFormType.LOGIN),
+	'/register': loginView(getFormType.REGISTER)
+}
 
-var item = document.querySelector("#app")!;
-item.innerHTML += loginView()
+const onChangePath = path => {
+	let content = routes[path]
+	if (content == undefined)
+		content = `<h1>Hello, world </h1> <a href="/login" >Login</a>`
+	APP.innerHTML = content
+}
+
+let pathName: string = window.location.pathname
+
+onChangePath(pathName)
+
--- app/src/pages/login.ts
+++ app/src/pages/login.ts
@@ -1,23 +1,67 @@
 import '../css/login.css'
 
 
-export function loginView(btn:string  = "Entrar", title: string = "Welcome back!"): string {
+export const getFormType = Object.freeze({
+  LOGIN: 1,
+  REGISTER: 2
+});
+
+
+type FORM_TYPE = 1 | 2;
+
+export function loginView(typeForm: FORM_TYPE): string {
+
+	if (typeForm === getFormType.LOGIN) {
+		var type: string = "login"
+		var loginClassCSS: string = "link_active"
+		var registerClassCSS: string = "link_deactive"
+		var title: string = "Welcome back!"
+		var btn: string = "Entrar"
+	}
+	else {
+		type = "register"
+		loginClassCSS = "link_deactive"
+		registerClassCSS = "link_active"
+		title = "Welcome aboard!"
+		btn = "Registar"
+	}
+
 	return `
 		<div id="div_container" >
 			<div class="welcome_msg">
 				<h1 class="h3">${title}</h1>
 				<div >
-					<a class="links" href="#">Old User</a>
+					<a class="${loginClassCSS}" href="/login">Old User</a>
 					<span>/</span>
-					<a class="links_deactive" href="#">New User</a>
+					<a class="${registerClassCSS}" href="/register">New User</a>
 				</div>
 			</div>
 
-			<form class="d-flex flex-column">
-				<input class="form-control custom-input" type="text" placeholder="Username" aria-label="default input example" minlength=3 maxlength=10 required>
-				<input type="password" id="inputPassword5" class="form-control custom-input" aria-describedby="passwordHelpBlock" placeholder="Password" required>
+			<form id="register_login_form" class="d-flex flex-column">
+				<input id="input-username" class="form-control custom-input" type="text" autofocus placeholder="Username" minlength=3 maxlength=10 required>
+				<input id="input-password" type="password" class="form-control custom-input" placeholder="Password" required>
+				<input id="type_of_input" value="${type}" type="hidden">
 				<input class="btn btn-primary" type="submit" value="${btn}">
-				</form>
+			</form>
 		</div>
 	`
+}
+
+export function processForm(): void {
+	const userFrom = document.querySelector<HTMLFormElement>("#register_login_form");
+	if (!userFrom)
+		return;
+
+	userFrom.addEventListener("submit", (event) => {
+		event.preventDefault()
+
+		const payload: Object = {
+			username: document.querySelector<HTMLInputElement>("#input-username")?.value,
+			password: document.querySelector<HTMLInputElement>("#input-password")?.value,
+			type: document.querySelector<HTMLInputElement>("#type_of_input")?.value
+		}
+
+		console.log(payload)
+	
+	})
 }
